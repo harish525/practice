8697277cc99a7ef554fe5d2e0fca77041e390aec
Harish <gadhariharish@gmail.com>
2019-07-26 07:59:34 +0530

    mutliple forms banners header

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,32 @@
 // tabs
-$(document).ready(function(){
-    $("a.nav-link.enterprises").click(function(){
-      $(".ondemand-booking").removeClass('show active');
-      $(".nav-tabs a.nav-link").removeClass('active');
-      $(".enterprises").addClass('show active');
-    });
-    $("a.nav-link.ondemand-booking.active").click(function(){
-      $(".enterprises").removeClass('show active');
-      $(".ondemand-booking").addClass('show active');
-    });
-  });
+// $(document).ready(function(){
+//     $("a.nav-link.enterprises").click(function(){
+//       $(".ondemand-booking").removeClass('show active');
+//       $(".nav-tabs a.nav-link").removeClass('active');
+//       $(".enterprises").addClass('show active');
+//     });
+//     $("a.nav-link.ondemand-booking.active").click(function(){
+//       $(".enterprises").removeClass('show active');
+//       $(".ondemand-booking").addClass('show active');
+//     });
+//   });
+
+$('.our-customers').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+});
